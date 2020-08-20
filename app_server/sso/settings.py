bcbf2bbc5aaa62f65d499566fa5baa4c84bd4180
Stephcyrille <stephcyril.sc@gmail.com>
2020-08-20 14:53:05 +0100

    Refactor login components

--- app_server/sso/settings.py
+++ app_server/sso/settings.py
@@ -123,3 +123,4 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+LOGIN_URL = '/auth/login/'
--- app_server/sso/urls.py
+++ app_server/sso/urls.py
@@ -2,15 +2,15 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 from simple_sso.sso_server.server import Server
-
+from .views import LoginView, LogoutView
 
 test_server = Server()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^cache/clearcache/', include('clearcache.urls')),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
-    url(r'^', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^auth/login/$', LoginView.as_view(), name="login"),
+    url(r'^auth/logout/$', LogoutView.as_view(), name="logout"),
+    url(r'^home/$', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
 
 urlpatterns += [
