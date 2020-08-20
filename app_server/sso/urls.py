3f750f25ab78ef4c6fe0774cab7d76023cd5b4d0
Stephcyrille <stephcyril.sc@gmail.com>
2020-08-20 15:07:34 +0100

    Setting redirect url to the client app url and then, creating home template in the client app

--- app_server/sso/urls.py
+++ app_server/sso/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.views.generic.base import TemplateView
 from simple_sso.sso_server.server import Server
 from .views import LoginView, LogoutView
 
@@ -10,7 +9,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^auth/login/$', LoginView.as_view(), name="login"),
     url(r'^auth/logout/$', LogoutView.as_view(), name="logout"),
-    url(r'^home/$', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
 
 urlpatterns += [
